4c6a5623cff4af5dc37608a3d290a9e4cbcdb5a6
135087 <135087@ssg.com>
2021-08-04 15:40:55 +0900

    getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,25 @@
-export default function Home() {
+export default function Home({ posts }) {
+	// console.log(posts);
 	return (
 		<div>
-			<h1>welcome</h1>
+			<h1>Welcome to My Blog</h1>
+			<ul>
+				{posts.map((post) => (
+					<li key={post.id}>{post.title}</li>
+				))}
+			</ul>
 		</div>
 	);
 }
+
+export const getServerSideProps = async () => {
+	const res = await fetch("https://jsonplaceholder.typicode.com/posts?_start=0&_end=10");
+	// const res = await fetch("http://localhost:8081/api/posts");
+	const posts = await res.json(); // json으로 인코딩
+
+	return {
+		props: {
+			posts,
+		}
+	};
+};
