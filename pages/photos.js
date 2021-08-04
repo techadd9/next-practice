faff190650e270c4d6191b58847d7cf1c3de6dbf
135087 <135087@ssg.com>
2021-08-04 15:44:33 +0900

    getStaticProps

--- pages/photos.js
+++ pages/photos.js
@@ -1,12 +1,33 @@
 import HeadInfo from "../components/HeadInfo";
+import Image from "next/image";
+import photosStyles from "../styles/Photos.module.css";
 
-const photos = () => {
+const photos = ({ photos }) => {
 	return (
 		<div>
 			<HeadInfo title="My Blog Photos" />
 			<h1>my photos</h1>
+			<ul className={photosStyles.photos}>
+				{photos.map((photo) => (
+					<li key={photo.id}>
+						<Image src={photo.thumbnailUrl} width={100} height={100} alt={photo.title} />
+						<span>{photo.title}</span>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
+};
+
+export const getStaticProps = async () => {
+	const res = await fetch("https://jsonplaceholder.typicode.com/photos?_start=0&_end=10");
+	const photos = await res.json();
+
+	return {
+		props: {
+			photos,
+		},
+	};
 };
 
 export default photos;
